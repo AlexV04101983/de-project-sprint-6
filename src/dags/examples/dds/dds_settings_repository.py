ddad4ea645a2bad31ffe42e4a12740442f41901a
AlexV04101983 <frag041083@gmail.com>
2025-10-23 20:32:19 +0300

    Add files via upload

--- src/dags/examples/dds/dds_settings_repository.py
+++ src/dags/examples/dds/dds_settings_repository.py
@@ -33,7 +33,7 @@ class DdsEtlSettingsRepository:
         with conn.cursor() as cur:
             cur.execute(
                 """
-                    INSERT INTO dds.srv_wf_settings(workflow_key, workflow_settings)
+                    INSERT INTO stg.srv_wf_settings(workflow_key, workflow_settings)
                     VALUES (%(etl_key)s, %(etl_setting)s)
                     ON CONFLICT (workflow_key) DO UPDATE
                     SET workflow_settings = EXCLUDED.workflow_settings;
